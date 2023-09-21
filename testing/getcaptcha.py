7bcc1c139fb29e764749db1157364b8d0dacbf31
Jooney <jhan756k@gmail.com>
2023-09-21 16:48:03 +0900

    Initial

---
+++ testing/getcaptcha.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+
+driver = webdriver.Chrome()
+driver.get('https://www.google.com/recaptcha/api2/demo')
+
+for x in range(1, 11):
+    try:
+        button = WebDriverWait(driver, 3).until(EC.element_to_be_clickable((By.CLASS_NAME, 'g-recaptcha'))).click()
+    except TimeoutException:
+        print("btn time")
+        break
+    
+    try: 
+        iframe = WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div[4]/iframe')))
+        driver.switch_to.frame(iframe)
+        img = WebDriverWait(driver, 3).until(
+            EC.visibility_of_element_located((By.XPATH, '//*[@id="rc-imageselect-target"]/table')))
+        img.screenshot('images/orig/captcha' + str(x) + '.png')
+        driver.switch_to.default_content()
+    except TimeoutException:
+        print("img time")
+        break
+
+    driver.refresh()
---
+++ testing/imgdetect.py
@@ -0,0 +1,26 @@
+from ultralytics import YOLO
+import cv2, os
+
+model = YOLO('yolov8m.pt')
+imgpath = r"images/orig/"
+
+for imgname in os.listdir(imgpath):
+    img = cv2.imread(imgpath + imgname)
+    results = model.predict(imgpath + imgname)
+    result = results[0]
+
+    for box in result.boxes:
+        class_id = result.names[box.cls[0].item()]
+        cords = box.xyxy[0].tolist()
+        cords = [round(x) for x in cords]
+        conf = round(box.conf[0].item(), 2)
+        print("Object type:", class_id)
+        print("Coordinates:", cords)
+        print("Probability:", conf)
+        print("---")
+
+        img = cv2.rectangle(img, (cords[0], cords[1]), (cords[2], cords[3]), (0, 0, 255), 2)
+        img = cv2.putText(img, class_id + " " + str(conf), (cords[0], cords[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(0)
